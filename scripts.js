0bc1a881ce9c3458a4e14671b3204965435460c9
Nicolo <lukakuchaidze@gmail.com>
2023-04-27 22:43:03 +0400

    Calculator

--- scripts.js
+++ scripts.js
@@ -2,8 +2,8 @@
 window.addEventListener("load", function () {
     const loader = document.getElementById("loader");
     loader.style.display = "none";
-  });
-  
+});
+
 // Slider
 var ListLi = document.querySelectorAll('#ul li');
 var raodenoba = ListLi.length;
@@ -306,35 +306,96 @@ function closeLightBox() {
 lightBoxContainer.addEventListener("click", closeLightBox);
 
 
+// POPUP
 
+// get popup elements
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('.close');
+const viewModalBtn = document.querySelector('.view-modal1');
 
+// get form elements
+const inputNumber = document.querySelector('#input-number');
+const submitBtn = document.querySelector('#submit-btn');
+const productTable = document.querySelector('#product-table tbody');
+const total = document.querySelector('#total');
 
-// popup
- 
+inputNumber.addEventListener('keydown', (event) => {
+    if (event.keyCode === 13) {
+    event.preventDefault();
+    submitBtn.click();
+    }
+    });
 
-const modal = document.querySelector(".view-modal")
-const modal1 = document.querySelector(".view-modal1")
-const formContainer = document.querySelector(".form-main-container");
-const closebtn = document.querySelector('.closebtn')
-modal.addEventListener("click", function () {
-    console.log('asdasd')
-    formContainer.style.setProperty("display", "flex", "important")
-    sidebar.style.zIndex= "-1"
-    formContainer.style.zIndex = "99999" 
-  
+// open popup when view modal button is clicked
+viewModalBtn.addEventListener('click', () => {
+    popup.style.display = 'block';
 });
 
-closebtn.addEventListener("click",function(){
-    formContainer.style.setProperty("display", "none", "important")
-})
-
-
-
-modal1.addEventListener("click", function () {
-    console.log('asdasd')
-    formContainer.style.setProperty("display", "flex", "important")
-    sidebar.style.zIndex= "-1"
-    formContainer.style.zIndex = "99999" 
-  
+// close popup when close button is clicked
+closeBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
 });
- 
+
+// close popup when user clicks outside of it
+window.addEventListener('click', (e) => {
+    if (e.target == popup) {
+        popup.style.display = 'none';
+    }
+});
+
+// function to calculate product price
+function calculatePrice(number, multiplier) {
+    return number * multiplier;
+}
+
+// function to generate table row
+function generateTableRow(product, price, rowNumber) {
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${product} ${rowNumber}</td>
+    <td class="price">${price} GEL</td>
+    <td><button class="disable">გამორთვა</button></td>
+  `;
+    const disableBtn = tr.querySelector('.disable');
+    disableBtn.addEventListener('click', () => {
+        tr.classList.toggle('disabled');
+        if (tr.classList.contains('disabled')) {
+            disableBtn.textContent = 'ჩართვა';
+            disableBtn.style.backgroundColor = '#4CAF50';
+        } else {
+            disableBtn.textContent = 'გამორთვა';
+            disableBtn.style.backgroundColor = '';
+        }
+        // recalculate total
+        let totalSum = 0;
+        const rows = productTable.querySelectorAll('tr:not(:first-child)');
+        rows.forEach((row) => {
+            if (!row.classList.contains('disabled')) {
+                const priceCell = row.querySelector('.price');
+                const price = parseFloat(priceCell.textContent);
+                totalSum += price;
+            }
+        });
+        total.textContent = `${totalSum} GEL`;
+    });
+    return tr;
+}
+
+// generate table rows when user submits form
+submitBtn.addEventListener('click', () => {
+    // remove existing table rows
+    while (productTable.firstChild) {
+        productTable.removeChild(productTable.firstChild);
+    }
+    // calculate and generate new table rows
+    let totalSum = 0;
+    for (let i = 1; i <= 8; i++) {
+        const product = 'პროდუქტი';
+        const price = calculatePrice(inputNumber.value, i);
+        const tr = generateTableRow(product, price, i);
+        productTable.appendChild(tr);
+        totalSum += price;
+    }
+    // update total
+    total.textContent = `${totalSum} GEL`;
+});
